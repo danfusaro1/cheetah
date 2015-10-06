7d77bfb40c78fa11d893401e223f780030781208
Your Actual Name <danfusaro1@gmail.com>
2015-10-06 16:31:52 -0400

    my first commit

---
+++ cheetah.rb
@@ -0,0 +1,30 @@
+CHEETAH_GIRLS = [
+"Raven-Symone",
+"Adrienne Bailon",
+"Sabrina Bryan",
+"Kiely Williams"
+]
+
+auditions = [
+"Raven-Symone",
+"Usher",
+"Wiz Khalifa",
+"Adrienne Bailon",
+"Hulk Hogan",
+"Sabrina Bryan",
+"Diego Lugo",
+"Kiely Williams",
+"Justin Timberlake"
+]
+
+panel = []
+
+#YOUR CODE HERE
+unless CHEETAH_GIRLS.include? auditions then
+    panel.push (CHEETAH_GIRLS)
+    
+    
+end
+
+puts panel
+
